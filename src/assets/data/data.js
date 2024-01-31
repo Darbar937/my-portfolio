73e5a82ae67d05b074f0983b622961eae438c59d
Darbar robin <119269461+Darbar937@users.noreply.github.com>
2024-01-31 09:55:27 +0530

    Add files via upload

---
+++ src/assets/data/data.js
@@ -0,0 +1,30 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img:'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
+    title: "Multimedia Web",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://myareanetwork-photos.s3.amazonaws.com/editorphotos/f/65033_1623697840.png',
+    title: "Food order website",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://themehunk.com/wp-content/uploads/2021/03/shoes-store-image.png',
+    title: "Ecommerce shoe store",
+    desc: "",
+    link: "",
+    git: "",
+  }
+  
+  
+  
+];
